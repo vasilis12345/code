b0fb9d9fc057976d2c66a4fcc79754faa0034585
vasilis12345 <168472236+vasilis12345@users.noreply.github.com>
2024-06-04 17:21:16 +0300

    new mini prroject

---
+++ sleep.py
@@ -0,0 +1,15 @@
+hours = input("how many hours do you sleep")
+
+number = float(float(hours))
+
+if number < 8:
+
+ print(f"{number} hours are too little")
+ 
+elif number > 10:
+ 
+ print(f"{number} hours is too much")
+ 
+else :
+   
+   print("you are sleeping enough")
