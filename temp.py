4539d247fc7868ceccf51388beb095fe815342f3
vasilis12345 <168472236+vasilis12345@users.noreply.github.com>
2024-06-04 17:49:58 +0300

    new

---
+++ temp.py
@@ -0,0 +1,14 @@
+print("This is a temprature convertor")
+print("It converts celcius to fahrenite and the opposite")
+
+temp_unit = input("what temprature unit do you want to convert? \n type 1 for celsius or 2 for fahrenite")
+temp_unit = float(float(temp_unit))
+
+degree = input("and how many degrees do you want to convert?")
+degree =float(float(degree))
+
+if temp_unit == 1:
+  degree_c_outcome = (degree * 9/5 + 32)
+  print(f"The result is {degree_c_outcome}")
+
+  
