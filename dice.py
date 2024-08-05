53765b7eb58a4dac7595eaf8a20c4af7dbfb4a9f
vasilis12345 <168472236+vasilis12345@users.noreply.github.com>
2024-08-05 23:45:23 +0300

    a

--- dice.py
+++ dice.py
@@ -1,25 +1,29 @@
 print("do you want to roll a dice?")
-
+choice = input("ansawer with 1 for yes and 2 for no")
 import random
 
 dice = (random.randint(6,6))
+if  choice == 1 :
 
-if dice == 1 : 
+ if dice == 1 : 
     print("   \n * \n   ")
     print(f"the outcome is {dice}")
-elif dice == 2 :
+ elif dice == 2 :
     print("*  \n  \n   *")
     print(f"the outcome is {dice}")
-elif dice == 3 :
+ elif dice == 3 :
     print("* \n * \n  *")
     print(f"the outcome is {dice}")
-elif dice == 4 :
+ elif dice == 4 :
     print("* *\n  \n* *")
     print(f"the outcome is {dice}")
-elif dice == 5 :
+ elif dice == 5 :
      print("* *\n * \n* *")
      print(f"the outcome is {dice}")
-else :
+ else :
      print("***\n***\n***")
      print(f"the outcome is {dice}")
 
+else :
+   print("okay then")
+   
