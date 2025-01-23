6ebc78828edad52448ddb84ec45a4496a4260f72
vasilis12345 <168472236+vasilis12345@users.noreply.github.com>
2025-01-23 23:56:20 +0200

    lol

---
+++ slots.py
@@ -0,0 +1,22 @@
+money = 1000
+
+import random 
+
+number1 = random.randint(1, 10) 
+number2 = random.randint(1, 10)
+number3 = random.randint(1, 10)
+print("wanna play a game of slots?")
+print("balance = " ,money)
+
+print(number1 , "|" , number2 , "|" , number3)
+
+roll = input("Do you want to roll? (type yes or no)")
+
+if roll == yes  
+    if number1 == number2 and number1 == number3
+        print("You won")
+        money + 25 
+else 
+
+print("Bye then")
+break          
