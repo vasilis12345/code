4965f4beeacb443cfebd69cd4fa38a89cb33e3fc
vasilis12345 <168472236+vasilis12345@users.noreply.github.com>
2025-01-26 00:49:49 +0200

    lol

--- slots.py
+++ slots.py
@@ -1,22 +1,32 @@
 money = 1000
+import random
 
-import random 
+print("Wanna play a game of slots?")
+print("Balance =", money)
 
-number1 = random.randint(1, 10) 
-number2 = random.randint(1, 10)
-number3 = random.randint(1, 10)
-print("wanna play a game of slots?")
-print("balance = " ,money)
+roll = int(input("Do you want to roll? (type 1 for yes or 2 for no): "))
+while money > 0 and roll == 1:
+    number1 = random.randint(1, 10)
+    number2 = random.randint(1, 10)
+    number3 = random.randint(1, 10)
+    
+    print(number1, "|", number2, "|", number3)
+    
+    if number1 == number2 and number1 == number3:
+        print("You won!")
+        money += 25
+    else:
+        print("You lost!")
+        money -= 25
+        print("Balance =", money)
+    
+    if money <= 0:
+        print("You're out of money. Game over!")
+        break
+    
+    roll = int(input("Do you want to re-roll? (type 1 for yes or 2 for no): "))
 
-print(number1 , "|" , number2 , "|" , number3)
+if money > 0:
+    print("Goodbye!")
 
-roll = input("Do you want to roll? (type yes or no)")
 
-if roll == yes  
-    if number1 == number2 and number1 == number3
-        print("You won")
-        money + 25 
-else 
-
-print("Bye then")
-break          
